5b21de271195b5945a98469a62e006e1f92c64fb
Marc Partensky <marc.partensky@gmail.com>
2022-08-07 15:33:32 +0200

    nvimtree + telescope

--- lua/marc/packer.lua
+++ lua/marc/packer.lua
@@ -11,4 +11,16 @@ return require('packer').startup(function()
   use 'b3nj5m1n/kommentary'
   use {'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'    }
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 end)
