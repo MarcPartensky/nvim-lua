5a2356336c54b8a1fe68547a7de3415b8c5da3eb
Marc Partensky <marc.partensky@gmail.com>
2022-10-05 16:12:20 +0200

    gl

--- lua/marc/remap.lua
+++ lua/marc/remap.lua
@@ -7,6 +7,7 @@ keymap('n', '<C-t>', ":tabnew<CR>", {})
 keymap('n', 'gp', ":!gp<cr>", {})
 keymap('n', 'gn', ":!gn ", {})
 keymap('n', 'gt', ":!gt ", {})
+keymap('n', 'gl', ":!gpl<cr>", {})
 -- selection
 keymap('n', '<c-a>', "ggVG", {})
 -- save
