814579cfb0ea83445d9f8bf54e5bfec796671b73
Marc Partensky <marc.partensky@gmail.com>
2022-10-05 16:08:59 +0200

    lowercases

--- lua/marc/remap.lua
+++ lua/marc/remap.lua
@@ -1,9 +1,14 @@
 -- local nnoremap = require("marc.keymap").nnoremap
 local keymap = vim.api.nvim_set_keymap
 
+-- tabs
 keymap('n', '<C-t>', ":tabnew<CR>", {})
-keymap('n', 'gp', ":!gp<CR>", {})
+-- git
+keymap('n', 'gp', ":!gp<cr>", {})
 keymap('n', 'gn', ":!gn ", {})
 keymap('n', 'gt', ":!gt ", {})
-keymap('n', '<F3>', ":!curl --silent ipconfig.io<CR>", {})
-keymap('n', '<F1>', ":PackerSync<CR>", {})
+keymap('n', '<c-s-insert>', "ggVG", {})
+keymap('n', '<c-s>', ":w<cr>", {})
+-- fn
+keymap('n', '<f1>', ":PackerSync<cr>", {})
+keymap('n', '<f3>', ":!curl --silent ipconfig.io<cr>", {})
