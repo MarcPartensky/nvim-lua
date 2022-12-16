be2416f63f8621c39310d23a44455c6c2573727b
Marc Partensky <marc.partensky@gmail.com>
2022-12-16 22:51:53 +0100

    back to bufferline

--- lua/marc/packer.lua
+++ lua/marc/packer.lua
@@ -45,8 +45,8 @@ return require('packer').startup(function()
   use({ "glepnir/lspsaga.nvim", branch = "main" })
 
   -- buffers (tabs)
-  -- use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
-  use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+  use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+  -- use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
 
   -- comment
   use {
@@ -58,9 +58,11 @@ return require('packer').startup(function()
 
   -- main theme
   use 'navarasu/onedark.nvim'
+
   -- transparent frontground
   use 'xiyaowong/nvim-transparent'
   -- use 'folke/tokyonight.nvim'
+  --
   -- smooth scroll with <C-d>
   use 'psliwka/vim-smoothie'
   -- integration with pywal
@@ -69,18 +71,20 @@ return require('packer').startup(function()
   use "windwp/nvim-autopairs"
   -- better make
   use 'neomake/neomake'
+
+  -- markdown
   use {'marcpartensky/markdown-preview.nvim', run = 'cd app && yarn install', cmd = 'MarkdownPreview'    }
   use {
-		"nvim-treesitter/nvim-treesitter",
-		-- :TSUpdate[Sync] doesn't exist until plugin/nvim-treesitter is loaded (i.e. not after first install); call update() directly
-		run = function() require("nvim-treesitter.install").update { with_sync = true } end
-    }
+	"nvim-treesitter/nvim-treesitter",
+	-- :TSUpdate[Sync] doesn't exist until plugin/nvim-treesitter is loaded (i.e. not after first install); call update() directly
+	run = function() require("nvim-treesitter.install").update { with_sync = true } end
+  }
     if packer_bootstrap then require("packer").sync() end
   -- navigation
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
     -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
   use {
     'nvim-tree/nvim-tree.lua',
@@ -89,4 +93,10 @@ return require('packer').startup(function()
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
 end)
