b36137730273bbc1d53e21b5f87d7e09f01087f7
Marc Partensky <marc.partensky@gmail.com>
2023-03-27 13:18:03 +0200

    nvim-tree new nvim bindings

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,13 +1,26 @@
 -- examples for your init.lua
 
--- empty setup using defaults
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+    view = {
+        adaptive_size = true,
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = "l", action = "edit", action_cb = edit_or_open },
+                { key = "L", action = "vsplit_preview", action_cb = vsplit_preview },
+                { key = "h", action = "close_node" },
+                { key = "H", action = "collapse_all", action_cb = collapse_all }
+            }
+        },
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true
+        }
+    }
 
--- OR setup with some options
--- require("nvim-tree").setup({
 --   sort_by = "case_sensitive",
 --   view = {
---     adaptive_size = true,
 --     mappings = {
 --       list = {
 --         { key = "u", action = "dir_up" },
@@ -20,5 +33,5 @@ require("nvim-tree").setup()
 --   filters = {
 --     dotfiles = true,
 --   },
--- })
+})
 vim.api.nvim_set_keymap('n', '<C-o>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
