b9de7e0075dd8df7bde3c9594a19959a9ad8eadf
Marc Partensky <marc.partensky@gmail.com>
2024-06-23 04:58:24 +0200

    clean

--- init.lua
+++ init.lua
@@ -1,16 +1,3 @@
--- local lspconfig = require('lspconfig')
-
--- -- TypeScript language server setup
--- lspconfig.tsserver.setup {}
-
--- lua_ls = {
---     Lua = {
---         workspace = { checkThirdParty = false },
---         telemetry = { enable = false },
---     },
--- }
---
-
 vim.opt.relativenumber = true
 
 require "marc.set"
