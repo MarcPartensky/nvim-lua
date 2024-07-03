a50feccdf1b203b8afc873dee321731528bf1f39
Marc Partensky <marc.partensky@gmail.com>
2024-07-03 02:06:59 +0200

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', 'fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
         vim.keymap.set('n', 'fb', builtin.buffers, {})
         vim.keymap.set('n', 'fh', builtin.help_tags, {})
         require('telescope').setup {
@@ -13,13 +13,13 @@ return {
             },
 
             -- open telescope instead of empty buffer
-            vim.api.nvim_create_autocmd("VimEnter", {
-                callback = function()
-                    if vim.fn.argv(0) == "" then
-                        require("telescope.builtin").find_files()
-                    end
-                end,
-            })
+            -- vim.api.nvim_create_autocmd("VimEnter", {
+            --     callback = function()
+            --         if vim.fn.argv(0) == "" then
+            --             require("telescope.builtin").find_files()
+            --         end
+            --     end,
+            -- })
         }
     end
 }
