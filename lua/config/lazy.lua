d5f9f738580744d29f178fd78b1d0d23ee9a3005
Marc Partensky <marc.partensky@gmail.com>
2024-07-03 14:37:27 +0200

    noice treesj

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,8 @@ require('lazy').setup({
         { import = "plugins.nvim-tree" },         -- tab for file explorer
         { import = "plugins.vim-smoothie" },      -- smooth cursor scroll
         { import = "plugins.comment" },           -- comment doesnt work
+        { import = "plugins.treesj" },            -- split/join blocks of code
+        -- { import = "plugins.noice" },             -- notifications
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
